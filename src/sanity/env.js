51f54dc69b76e4c881920fddfab95887bc14fef1
Orpheus Grant Essilfie <swissagem11@gmail.com>
2026-08-17 12:38:37 +0000

    Hardcode Sanity projectId to fix Vercel crash

--- src/sanity/env.js
+++ src/sanity/env.js
@@ -1,5 +1,8 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-08-16'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-03-01'
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const dataset = 'production'
+
+export const projectId = 'x0tpoga9'
+
+export const useCdn = false
